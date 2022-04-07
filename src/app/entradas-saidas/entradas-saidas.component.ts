68373cb4f9b1b2432c6f89627ce50ccdeaabb69c
tsm13 <thais@live.jp>
2022-04-07 12:35:06 -0300

    consertando path entradas/saidas

---
+++ src/app/entradas-saidas/entradas-saidas.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-entradas-saidas',
+  templateUrl: './entradas-saidas.component.html',
+  styleUrls: ['./entradas-saidas.component.scss'],
+})
+export class EntradasSaidasComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
