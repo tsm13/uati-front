a957ce7cf401f5a6c40b1e914a42393beb4f3959
tsm13 <thais@live.jp>
2022-04-08 14:29:25 -0300

    editando tabela entradas-saidas

--- src/app/entradas-saidas/entradas-saidas.component.ts
+++ src/app/entradas-saidas/entradas-saidas.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 
 export interface TabelaExtrato {
@@ -58,40 +58,4 @@ export class EntradasSaidasComponent {
 
   colunas: string[] = ['data', 'lancamentos', 'valor', 'saldo', 'detalhes'];
   dataSource = new MatTableDataSource(DADOS);
-
-  if() {}
 }
-
-/*
-import { Component } from '@angular/core';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
-
-
- @Component({
-  selector: 'app-entradas-saidas',
-  templateUrl: './entradas-saidas.component.html',
-  styleUrls: ['./entradas-saidas.component.scss'],
-})
-export class EntradasSaidasComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
-} */
