1bd1a2dbcea98e33330afcac05ae61b7be69b14b
tsm13 <thais@live.jp>
2022-04-08 13:53:42 -0300

    editando tabela entradas-saidas

--- src/app/entradas-saidas/entradas-saidas.component.ts
+++ src/app/entradas-saidas/entradas-saidas.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface TabelaExtrato {
   data: string;
@@ -10,10 +11,38 @@ export interface TabelaExtrato {
 
 const DADOS: TabelaExtrato[] = [
   {
+    data: '15/08/2021',
+    lancamentos: 'Saldo anterior',
+    valor: '',
+    saldo: '39.533,94',
+    detalhes: '',
+  },
+  {
     data: '19/08/2021',
     lancamentos: 'Pai pgto automatico itau',
-    valor: '2679,00',
+    valor: '- 2.679,00',
     saldo: '',
+    detalhes: '',
+  },
+  {
+    data: '19/08/2021',
+    lancamentos: 'Resgate RDB/ CDB',
+    valor: '5.087,00',
+    saldo: '',
+    detalhes: '',
+  },
+  {
+    data: '23/08/2021',
+    lancamentos: 'Pagamento cheque 002398',
+    valor: '- 9.368,00',
+    saldo: '',
+    detalhes: '',
+  },
+  {
+    data: '24/08/2021',
+    lancamentos: 'Saldo do dia',
+    valor: '',
+    saldo: '32.573,94',
     detalhes: '',
   },
 ];
@@ -22,12 +51,15 @@ const DADOS: TabelaExtrato[] = [
   selector: 'app-entradas-saidas',
   templateUrl: './entradas-saidas.component.html',
   styleUrls: ['./entradas-saidas.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class EntradasSaidasComponent {
   constructor() {}
 
   colunas: string[] = ['data', 'lancamentos', 'valor', 'saldo', 'detalhes'];
-  dataSource = DADOS;
+  dataSource = new MatTableDataSource(DADOS);
+
+  if() {}
 }
 
 /*
