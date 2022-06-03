ed7f29b61c18159aa8f2991d12e63517270b1cdc
tsm13 <thais@live.jp>
2022-06-03 18:25:18 -0300

    Criando tela de erro

---
+++ src/app/shared/tela-erro/tela-erro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-erro',
+  templateUrl: './tela-erro.component.html',
+  styleUrls: ['./tela-erro.component.scss']
+})
+export class TelaErroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
